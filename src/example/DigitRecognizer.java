7250ae6f80173e7c74cd961afc919051171c12c8
Abhay D <a.deshpande012@gmail.com>
2017-02-06 23:43:37 -0800

    Commiting more code for DigitRecognizer

--- src/example/DigitRecognizer.java
+++ src/example/DigitRecognizer.java
@@ -3,6 +3,7 @@ package example;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
@@ -18,14 +19,58 @@ public class DigitRecognizer {
 		
 	}
 	
-	
+	private NeuralNetwork network;
 	public DigitRecognizer(String path){
 		File folder = new File(path);
-		File[] images = folder.listFiles();
-		if(images == null){
+		if(!folder.isDirectory()){
 			System.err.println("Supplied path is not a folder!");
 			return;
 		}
+		File[] images = folder.listFiles();
+		ArrayList<double[]> inputList = new ArrayList<double[]>();
+		ArrayList<Double> outputList = new ArrayList<Double>();
+		for(File file:images){
+			if(!file.getName().endsWith("jpg") && !file.getName().endsWith("png") && !file.getName().endsWith("jpeg")) continue;
+			try {
+				BufferedImage img = ImageUtils.toBufferedImage(ImageIO.read(file).getScaledInstance(WIDTH, HEIGHT, BufferedImage.SCALE_FAST));
+				ImageUtils.monoColor(img);
+				double[] input = getDataFromBufferedImage(img);
+				String result = file.getName().substring(file.getName().lastIndexOf(".")-3,file.getName().lastIndexOf("."));
+				outputList.add(Double.parseDouble(result));
+				inputList.add(input);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		double[][] inputs = inputList.toArray(new double[0][0]);
+		double[] outputs = arrayListToArray(outputList);
+		network = new NeuralNetwork(new int[]{WIDTH*HEIGHT,WIDTH*HEIGHT/2,1});
+		network.train(inputs, outputs, 0.25);
+	}
+	
+	public int guess(String path) throws IOException{
+		BufferedImage image = ImageUtils.toBufferedImage(ImageIO.read(new File(path)).getScaledInstance(WIDTH, HEIGHT, BufferedImage.SCALE_FAST));
+		double[] input = getDataFromBufferedImage(image);
+		double result = network.guess(input);
+		return (int) (result*10);
+	}
+	
+	double[] arrayListToArray(ArrayList<Double> list){
+		double[] toReturn = new double[list.size()];
+		for(int i = 0; i < list.size(); i++){
+			toReturn[i] = list.get(i);
+		}
+		return toReturn;
+	}
+	
+	double[] getDataFromBufferedImage(BufferedImage img){
+		double[] toReturn = new double[img.getHeight()*img.getWidth()];
+		for(int i = 0; i < img.getWidth(); i++){
+			for(int j = 0; j < img.getHeight(); j++){
+				toReturn[img.getWidth()*j+i] = img.getRGB(i, j);
+			}
+		}
+		return toReturn;
 	}
 	
 	void showImage(BufferedImage img){
--- src/example/ImageUtils.java
+++ src/example/ImageUtils.java
@@ -1,6 +1,8 @@
 package example;
 
 import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -30,5 +32,24 @@ public class ImageUtils {
 				img.setRGB(i,j, mono.getRGB());
 			}
 		}
+	}
+	
+	public static BufferedImage toBufferedImage(Image img)
+	{
+	    if (img instanceof BufferedImage)
+	    {
+	        return (BufferedImage) img;
+	    }
+
+	    // Create a buffered image with transparency
+	    BufferedImage bimage = new BufferedImage(img.getWidth(null), img.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+
+	    // Draw the image on to the buffered image
+	    Graphics2D bGr = bimage.createGraphics();
+	    bGr.drawImage(img, 0, 0, null);
+	    bGr.dispose();
+
+	    // Return the buffered image
+	    return bimage;
 	}
 }
