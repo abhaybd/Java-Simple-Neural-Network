cebfaa8a0141c23f0cb7565479defb92e58a8923
Abhay D <a.deshpande012@gmail.com>
2017-02-06 21:45:01 -0800

    Partial code commit of DigitRecognizer

--- src/example/DigitRecognizer.java
+++ src/example/DigitRecognizer.java
@@ -9,20 +9,30 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
+import neuralnetwork.NeuralNetwork;
+
 public class DigitRecognizer {
+	public static final int WIDTH = 28;
+	public static final int HEIGHT = 28;
 	public static void main(String[] args) throws IOException{
-		new DigitRecognizer(ImageIO.read(new File("data/butterfly.jpg")));
+		
 	}
 	
-	public DigitRecognizer(BufferedImage image){
-		showImage(image);
+	
+	public DigitRecognizer(String path){
+		File folder = new File(path);
+		File[] images = folder.listFiles();
+		if(images == null){
+			System.err.println("Supplied path is not a folder!");
+			return;
+		}
 	}
 	
 	void showImage(BufferedImage img){
 		JFrame frame = new JFrame();
 		JLabel label = new JLabel();
 		ImageUtils.monoColor(img);
-		label.setIcon(new ImageIcon(img.getScaledInstance(28, 28, BufferedImage.SCALE_FAST).getScaledInstance(280, 280, BufferedImage.SCALE_FAST)));
+		label.setIcon(new ImageIcon(img.getScaledInstance(WIDTH, HEIGHT, BufferedImage.SCALE_FAST).getScaledInstance(WIDTH*10, HEIGHT*10, BufferedImage.SCALE_FAST)));
 		frame.getContentPane().add(label);
 		frame.pack();
 		frame.setVisible(true);
